d5030303e013cdf735494403815faf235558e132
Felix Viola <felix.viola@uni-bonn.de>
2024-05-06 15:59:03 +0200

    Reset sim space sizing

--- artificial-life/conway/conway.js
+++ artificial-life/conway/conway.js
@@ -2,10 +2,8 @@ const startStopBtn = document.getElementById("startStopBtn")
 const stepButton = document.getElementById("stepButton")
 const resetBtn = document.getElementById("resetBtn")
 
-// const width = 101;
-// const height = 82;
-const width = 200;
-const height = 160;
+const width = 101;
+const height = 82;
 const cell_size = 8;
 
 let cells = []
